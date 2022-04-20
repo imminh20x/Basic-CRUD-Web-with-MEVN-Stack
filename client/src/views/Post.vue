1fbfc2cac648cb6b67d52a08444bc977cd927bd4
Đỗ Cao Minh <caominhbaby010@gmail.com>
2022-04-20 15:07:18 +0700

    Add update and delete post

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -11,18 +11,23 @@
                   {{ post.category }}
                 </v-btn>
               </v-col>
-              <v-col sm='10' class='d-flex justify-end'>
-                  <v-btn color='success' text>Edit</v-btn>
-                  <v-btn color='red' text>Delete</v-btn>
+              <v-col sm="10" class="d-flex justify-end">
+                <v-btn
+                  color="success"
+                  text
+                  :to="{ name: 'edit-post', params: { id: post._id } }"
+                  >Edit</v-btn
+                >
+                <v-btn color="red" text @click="removePost(post._id)">Delete</v-btn>
               </v-col>
             </v-row>
           </v-card-actions>
-          <v-card-subtitle class='headline'>
+          <v-card-subtitle class="headline">
             {{ post.title }}
           </v-card-subtitle>
-          <v-card-text class='grey--text'>
-              <p>{{post.content}}</p>
-              <p>{{post.created}}</p>
+          <v-card-text class="grey--text">
+            <p>{{ post.content }}</p>
+            <p>{{ post.created }}</p>
           </v-card-text>
         </v-card>
       </v-col>
@@ -41,6 +46,12 @@ export default {
   async created() {
     const response = await API.getPostByID(this.$route.params.id);
     this.post = response;
+  },
+  methods: {
+    async removePost(id) {
+      const response = await API.deletePost(id);
+      this.$router.push({ name: "home", params: {message: response.message} });
+    },
   },
 };
 </script>
